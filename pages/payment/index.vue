ba7c7c59f1864cbfcdc12c10894e4914144b9f3a
umayanga1997 <umayanga.avi@gmail.com>
2022-06-17 12:10:14 +0530

    ipay intergrated

--- pages/payment/index.vue
+++ pages/payment/index.vue
@@ -1,8 +1,61 @@
 <template>
   <loading-compo v-if="loading" />
   <div v-else>
-    <v-col cols="12" md="6" lg="4" sm="12" id="card_container"></v-col>
-    <v-btn @click="addata()"> Add Data</v-btn>
+    <form method="POST" action="https://sandbox.ipay.lk/ipg/checkout">
+      <input
+        type="hidden"
+        name="merchantWebToken"
+        value="eyJhbGciOiJIUzUxMiJ9.eyJtaWQiOiIwMDAwMDIxNSJ9.ERTtW8gyLPl5dw39cUaO08kIuNZJvTH-hgfGG1Wh4j9vDHjKuaFanJMqW8Mi30o5aHwY86ZappNtpQ7GkEvrvQ"
+      />
+      <!-- Replace your web token -->
+      <input type="hidden" name="orderId" value="OID123456" />
+      <input type="hidden" name="orderDescription" value="My Order" />
+      <!-- Optional -->
+      <input
+        type="hidden"
+        name="returnUrl"
+        value="http://mywebsite.com/return?orderId=OID123456"
+      />
+      <input
+        type="hidden"
+        name="cancelUrl"
+        value="http://mywebsite.com/cancel?orderId=OID123456"
+      />
+      <input type="hidden" name="subMerchantReference" value="" />
+      <!-- Optional -->
+      <!--<input type="hidden" name="paymentMethod" value="VISA"> -->
+      <table>
+        <tr>
+          <td>Total Amount</td>
+          <td>:</td>
+          <td><input type="text" name="totalAmount" value="750" /></td>
+        </tr>
+        <tr>
+          <td>Customer Name</td>
+          <td>:</td>
+          <td>
+            <input type="text" name="customerName" value="Ravindu Fernando" />
+          </td>
+        </tr>
+        <tr>
+          <td>Customer Mobile</td>
+          <td>:</td>
+          <td><input type="text" name="customerPhone" value="0701234567" /></td>
+        </tr>
+        <tr>
+          <td>Customer Email</td>
+          <td>:</td>
+          <td>
+            <input type="text" name="customerEmail" value="myemail@mail.com" />
+          </td>
+        </tr>
+      </table>
+      <br />
+      <input type="submit" value="Checkout Now" />
+    </form>
+
+    <!-- <v-col cols="12" md="6" lg="4" sm="12" id="card_container"></v-col>
+    <v-btn @click="addata()"> Add Data</v-btn> -->
   </div>
 </template>
 
@@ -69,7 +122,8 @@ export default {
             cors: true,
             headers: {
               "Access-Control-Allow-Origin": "*",
-              'merchantWebToken':'eyJhbGciOiJIUzUxMiJ9.eyJtaWQiOiIwMDAwMDIxNSJ9.ERTtW8gyLPl5dw39cUaO08kIuNZJvTH-hgfGG1Wh4j9vDHjKuaFanJMqW8Mi30o5aHwY86ZappNtpQ7GkEvrvQ'
+              merchantWebToken:
+                "eyJhbGciOiJIUzUxMiJ9.eyJtaWQiOiIwMDAwMDIxNSJ9.ERTtW8gyLPl5dw39cUaO08kIuNZJvTH-hgfGG1Wh4j9vDHjKuaFanJMqW8Mi30o5aHwY86ZappNtpQ7GkEvrvQ",
             },
           }
         );
@@ -239,5 +293,9 @@ export default {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+}
+.img-merchant-logo {
+  width: 100px !important;
+  height: 100px !important;
 }
 </style>
