f93c48100bf93b7e1fbea38d7a44267dce5a6059
umayanga1997 <umayanga.avi@gmail.com>
2022-06-19 14:21:54 +0530

    modified and bug fixed

--- components/buy-btn.vue
+++ components/buy-btn.vue
@@ -7,7 +7,17 @@
     />
     <!-- Replace your web token -->
     <input type="hidden" name="orderId" :value="orderId" />
-    <input type="hidden" name="orderDescription" value="My Order" />
+    <input
+      type="hidden"
+      name="orderDescription"
+      :value="
+        this.item_type == 'video'
+          ? 'Bought a video lesson'
+          : this.item_type == 'test'
+          ? 'Bought a test'
+          : 'Bought a tute'
+      "
+    />
     <!-- Optional -->
     <input
       type="hidden"
@@ -61,11 +71,20 @@
       <tr>
         <!-- id = item ID -->
         <!-- type -->
-        <!-- student_id -->
         <td>Custom Token Parameter</td>
         <td>:</td>
         <td>
           <input type="text" name="merchantParam1" :value="token" />
+        </td>
+      </tr>
+      <tr>
+        <!-- student_id -->
+        <!-- student_name -->
+        <!-- teacher_id -->
+        <td>Custom Token Parameter 2</td>
+        <td>:</td>
+        <td>
+          <input type="text" name="merchantParam2" :value="token2" />
         </td>
       </tr>
     </table>
@@ -84,6 +103,7 @@ export default {
     return {
       orderId: null,
       token: null,
+      token2: null,
     };
   },
   created() {
@@ -100,15 +120,17 @@ export default {
       this.token = jwt.sign(
         JSON.stringify({
           id: this.item.id,
-          type:
-            this.item_type == "video"
-              ? "Video"
-              : this.item_type == "test"
-              ? "Test"
-              : "Tute",
-          student_id: this.userData.student_id,
+          type: this.item_type,
         }),
         "buy_item"
+      );
+      this.token2 = jwt.sign(
+        JSON.stringify({
+          student_id: this.userData.student_id,
+          student_name: this.userData.name,
+          teacher_id: this.item.teacher_id,
+        }),
+        "s_t_data"
       );
     },
   },
